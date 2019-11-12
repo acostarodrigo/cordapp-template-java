046dcabe7e7d4d7c40e5b7edcdb462f41eb9be6f
rodrigo <acosta_rodrigo@hotmail.com>
2019-11-12 20:58:13 -0300

    Modified issueFlow of Arrangements to include the new CommercialPaperToken

--- workflows/src/main/java/org/shield/flows/arrangement/IssueFlow.java
+++ workflows/src/main/java/org/shield/flows/arrangement/IssueFlow.java
@@ -10,6 +10,7 @@ import net.corda.core.transactions.SignedTransaction;
 import net.corda.core.transactions.TransactionBuilder;
 import org.shield.contracts.ArrangementContract;
 import org.shield.contracts.CommercialPaperContract;
+import org.shield.flows.commercialPaper.CommercialPaperTokenFlow;
 import org.shield.states.ArrangementState;
 import org.shield.states.CommercialPaperState;
 
@@ -23,7 +24,6 @@ import java.util.UUID;
 public class IssueFlow  extends FlowLogic<UniqueIdentifier> {
     private UniqueIdentifier id;
     private ArrangementState input;
-    private CommercialPaperState commercialPaperState;
     private Party issuer;
     private Party brokerDealer;
     private StateAndRef<ArrangementState> inputArrangement;
@@ -46,31 +46,18 @@ public class IssueFlow  extends FlowLogic<UniqueIdentifier> {
             }
         }
 
+        // we can't go on if we don't have it.
         if (this.input == null)throw new FlowException("Arrangement with id " + this.id.toString() + " doesn't exists.");
 
         this.brokerDealer = this.input.getBrokerDealer();
 
-        // we have the arrangement, now we need to validate if we have a commercial paper for it.
-        for (StateAndRef<CommercialPaperState> stateAndRef : getServiceHub ().getVaultService().queryBy(CommercialPaperState.class).getStates()){
-            if (stateAndRef.getState().getData().getOfferingDate().equals(input.getOfferingDate())){
-                commercialPaperState = stateAndRef.getState().getData();
-                inputCommercialPaper = stateAndRef;
-            }
-        }
-        // we couldn't find a matching commercial paper, we will create it.
-        if (commercialPaperState == null){
-            UniqueIdentifier paperId = new UniqueIdentifier(CommercialPaperState.externalKey, UUID.randomUUID());
-            commercialPaperState = new CommercialPaperState(paperId, this.issuer, input.getSize(), input.getOfferingDate());
-        } else {
-            int currentSize = commercialPaperState.getSize();
-            int newSize = currentSize + input.getSize();
-            commercialPaperState.setSize(newSize);
-        }
-
+        // we issue the tokens.
+        CommercialPaperTokenFlow.IssueFungibleToken issueFungibleToken = new CommercialPaperTokenFlow.IssueFungibleToken(input.getOfferingDate(), new Long(input.getSize()), this.brokerDealer);
+        UniqueIdentifier paperId = subFlow(issueFungibleToken);
 
         // now we generate the new output of the arrangement with the commercialPaper id attached.
         ArrangementState output = this.input;
-        output.setPaperId(this.commercialPaperState.getId());
+        output.setPaperId(paperId);
         output.setState(ArrangementState.State.ISSUED);
 
         // it is time to generate the transaction.
@@ -84,12 +71,7 @@ public class IssueFlow  extends FlowLogic<UniqueIdentifier> {
         TransactionBuilder txBuilder = new TransactionBuilder(notary)
                 .addInputState(inputArrangement)
                 .addOutputState(output, ArrangementContract.ID)
-                .addOutputState(commercialPaperState, CommercialPaperContract.ID)
                 .addCommand(command);
-
-        // if we already had a commercial paper, we add the input
-        if (inputCommercialPaper != null)
-            txBuilder.addInputState(inputCommercialPaper);
 
         // Signing the transaction.
         SignedTransaction signedTx = getServiceHub().signInitialTransaction(txBuilder);
@@ -100,6 +82,6 @@ public class IssueFlow  extends FlowLogic<UniqueIdentifier> {
 
         // Finalising the transaction.
         subFlow(new FinalityFlow(fullySignedTx, brokerDealerSession));
-        return commercialPaperState.getId();
+        return paperId;
     }
 }
