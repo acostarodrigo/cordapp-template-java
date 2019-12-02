0dc3d72426a84f8c40ef7667f5285ddf984b1455
rodrigo <acosta_rodrigo@hotmail.com>
2019-12-02 11:09:20 -0300

    Added GracefullReconnect method to reconnect the RPC client

--- clients/src/main/java/org/shield/webserver/connection/NodeRPCConnection.java
+++ clients/src/main/java/org/shield/webserver/connection/NodeRPCConnection.java
@@ -1,11 +1,15 @@
 package org.shield.webserver.connection;
 
 import net.corda.client.rpc.CordaRPCClient;
+import net.corda.client.rpc.CordaRPCClientConfiguration;
 import net.corda.client.rpc.CordaRPCConnection;
+import net.corda.client.rpc.GracefulReconnect;
 import net.corda.core.messaging.CordaRPCOps;
 import net.corda.core.utilities.NetworkHostAndPort;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
 
 public class NodeRPCConnection {
     private static final Logger logger = LoggerFactory.getLogger(NodeRPCConnection.class);
@@ -29,9 +33,15 @@ public class NodeRPCConnection {
 
     public void initialiseNodeRPCConnection() {
         NetworkHostAndPort rpcAddress = new NetworkHostAndPort(host, port);
-        CordaRPCClient rpcClient = new CordaRPCClient(rpcAddress);
+        CordaRPCClientConfiguration configuration = new CordaRPCClientConfiguration(Duration.ofMinutes(3), 4, java.lang.Boolean.getBoolean("net.corda.client.rpc.trackRpcCallSites"), Duration.ofSeconds(1), 4, 1, Duration.ofSeconds(5), new Double(1), 10, 10485760, Duration.ofDays(1));
+        CordaRPCClient rpcClient = new CordaRPCClient(rpcAddress, configuration);
+
         logger.debug("Connecting to node " + host + ":" + port + " with user " + username);
-        rpcConnection = rpcClient.start(username, password);
+
+         GracefulReconnect gracefulReconnect = new GracefulReconnect(
+            () -> logger.info("on disconnect"), //todo implement
+            () -> logger.info("on reconnect"));
+        rpcConnection = rpcClient.start(username, password, gracefulReconnect);
         logger.debug("connected to node.");
         proxy = rpcConnection.getProxy();
     }
