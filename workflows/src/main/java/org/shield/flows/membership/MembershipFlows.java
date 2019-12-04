5182fc4afc3245c1b33911b6f87bdff2051c0776
rodrigo <acosta_rodrigo@hotmail.com>
2019-12-04 10:56:32 -0300

    added all flows for membership

--- workflows/src/main/java/org/shield/flows/membership/MembershipFlows.java
+++ workflows/src/main/java/org/shield/flows/membership/MembershipFlows.java
@@ -1,23 +1,33 @@
 package org.shield.flows.membership;
 
 import co.paralleluniverse.fibers.Suspendable;
+import com.r3.businessnetworks.membership.flows.bno.RequestMembershipFlowResponder;
 import com.r3.businessnetworks.membership.flows.member.GetMembershipsFlow;
+import com.r3.businessnetworks.membership.flows.member.RequestMembershipFlow;
 import com.r3.businessnetworks.membership.states.MembershipState;
-import com.sun.tools.javac.comp.Flow;
+import kotlin.UNINITIALIZED_VALUE;
+import kotlin.Unit;
 import net.corda.core.contracts.StateAndRef;
 import net.corda.core.cordapp.CordappConfig;
 import net.corda.core.flows.FlowException;
 import net.corda.core.flows.FlowLogic;
+import net.corda.core.flows.FlowSession;
 import net.corda.core.flows.InitiatedBy;
 import net.corda.core.identity.CordaX500Name;
 import net.corda.core.identity.Party;
-import org.shield.flows.arrangement.ArrangementFlow;
+import net.corda.core.transactions.TransactionBuilder;
+import org.jetbrains.annotations.NotNull;
 import org.shield.membership.ShieldMetadata;
+
+import static net.corda.core.contracts.ContractsDSL.requireThat;
 
 /**
  * Deals with all role and membership validations
  */
 public class MembershipFlows {
+    // we don't allow instantiation
+    private MembershipFlows(){};
+
     /**
      * Validates if the caller is a member of an Issuer organization.
      * Will return true if:
@@ -44,11 +54,112 @@ public class MembershipFlows {
     }
 
     /**
+     * Validates if the caller is a member of an Issuer organization.
+     * Will return true if:
+     *  is an active member
+     *  is a OrgType.BOND_PARTICIPANT
+     *  is a BondRole.BUYER
+     */
+    public static class isBuyer extends FlowLogic<Boolean>{
+        @Override
+        @Suspendable
+        public Boolean call() throws FlowException {
+
+            MembershipState membershipState = subFlow(new getMembership());
+
+            // we will validate is an active member of the organization
+            if (membershipState == null || !membershipState.isActive()) return false;
+
+            // node must be bond participant and bond issuer
+            ShieldMetadata metadata = (ShieldMetadata) membershipState.getMembershipMetadata();
+            if (metadata.getOrgTypes().contains(ShieldMetadata.OrgType.BOND_PARTICIPANT) && metadata.getBondRoles().contains(ShieldMetadata.BondRole.BUYER)) return true;
+
+            return false;
+        }
+    }
+
+    /**
+     * Validates if the caller is a member of an Issuer organization.
+     * Will return true if:
+     *  is an active member
+     *  is a OrgType.BOND_PARTICIPANT
+     *  is a BondRole.SELLER
+     */
+    public static class isSeller extends FlowLogic<Boolean>{
+        @Override
+        @Suspendable
+        public Boolean call() throws FlowException {
+
+            MembershipState membershipState = subFlow(new getMembership());
+
+            // we will validate is an active member of the organization
+            if (membershipState == null || !membershipState.isActive()) return false;
+
+            // node must be bond participant and bond issuer
+            ShieldMetadata metadata = (ShieldMetadata) membershipState.getMembershipMetadata();
+            if (metadata.getOrgTypes().contains(ShieldMetadata.OrgType.BOND_PARTICIPANT) && metadata.getBondRoles().contains(ShieldMetadata.BondRole.SELLER)) return true;
+
+            return false;
+        }
+    }
+
+    /**
+     * Validates if the caller is a member of a custodian organization.
+     * Will return true if:
+     *  is an active member
+     *  is a OrgType.CUSTODIAN
+     *
+     */
+    public static class isCustodian extends FlowLogic<Boolean>{
+        @Override
+        @Suspendable
+        public Boolean call() throws FlowException {
+
+            MembershipState membershipState = subFlow(new getMembership());
+
+            // we will validate is an active member of the organization
+            if (membershipState == null || !membershipState.isActive()) return false;
+
+            // node must be bond participant and bond issuer
+            ShieldMetadata metadata = (ShieldMetadata) membershipState.getMembershipMetadata();
+            if (metadata.getOrgTypes().contains(ShieldMetadata.OrgType.CUSTODIAN)) return true;
+
+            return false;
+        }
+    }
+
+    /**
+     * Validates if the caller is a member of a network treasurer organization.
+     * Will return true if:
+     *  is an active member
+     *  is a OrgType.NETWORK_TREASURER
+     *
+     */
+    public static class isTreasure extends FlowLogic<Boolean>{
+        @Override
+        @Suspendable
+        public Boolean call() throws FlowException {
+
+            MembershipState membershipState = subFlow(new getMembership());
+
+            // we will validate is an active member of the organization
+            if (membershipState == null || !membershipState.isActive()) return false;
+
+            // node must be bond participant and bond issuer
+            ShieldMetadata metadata = (ShieldMetadata) membershipState.getMembershipMetadata();
+            if (metadata.getOrgTypes().contains(ShieldMetadata.OrgType.NETWORK_TREASURER)) return true;
+
+            return false;
+        }
+    }
+
+    /**
      * Retrieves the membership state from the BNO node.
      * Will fail if caller is not a member
      */
-    private static class getMembership extends FlowLogic<MembershipState<? extends Object>>{
+    public static class getMembership extends FlowLogic<MembershipState<? extends Object>>{
         @Override
+        @Suspendable
         public MembershipState<? extends Object> call() throws FlowException {
             Party caller = getOurIdentity();
 
@@ -76,6 +187,7 @@ public class MembershipFlows {
 
             CordaX500Name bnoName = CordaX500Name.parse(bnoString);
             Party bno = getServiceHub().getNetworkMapCache().getPeerByLegalName(bnoName);
+
             return bno;
         }
     }
