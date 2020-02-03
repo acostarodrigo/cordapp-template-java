1f8b09d011bbda684022681428e18c465ef1331d
rodrigo <acosta_rodrigo@hotmail.com>
2020-02-03 14:44:57 -0300

    fixed route

--- config/dev/Azure/startNodes.sh
+++ config/dev/Azure/startNodes.sh
@@ -1,7 +1,7 @@
-cp build/resources/main/membership-service.conf ../../../build/nodes/issuer/cordapp/config
-cp build/resources/main/membership-service.conf ../../../build/nodes/trader1/cordapp/config
-cp build/resources/main/membership-service.conf ../../../build/nodes/trader2/cordapp/config
-cp build/resources/main/membership-service.conf ../../../build/nodes/BNO/cordapp/config
+cp ../../../build/resources/main/membership-service.conf ../../../build/nodes/issuer/cordapps/config/
+cp ../../../build/resources/main/membership-service.conf ../../../build/nodes/trader1/cordapps/config/
+cp ../../../build/resources/main/membership-service.conf ../../../build/nodes/trader2/cordapps/config/
+cp ../../../build/resources/main/membership-service.conf ../../../build/nodes/BNO/cordapps/config/
 sudo systemctl start cordaIssuer
 sudo systemctl start cordaTrader1
 sudo systemctl start cordaTrader2
