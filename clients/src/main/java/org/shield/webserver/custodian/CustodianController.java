5bb41fa944ee8e1d80d15d8e2379ce3c2e0a70ad
rodrigo <acosta_rodrigo@hotmail.com>
2020-04-20 17:13:24 -0300

    added new settlement blotter view on the REST controller.

--- clients/src/main/java/org/shield/webserver/custodian/CustodianController.java
+++ clients/src/main/java/org/shield/webserver/custodian/CustodianController.java
@@ -9,6 +9,7 @@ import net.corda.core.messaging.CordaRPCOps;
 import org.jetbrains.annotations.NotNull;
 import org.shield.custodian.CustodianState;
 import org.shield.offer.OfferState;
+import org.shield.trade.TradeState;
 import org.shield.webserver.connection.Connection;
 import org.shield.webserver.connection.ProxyEntry;
 import org.shield.webserver.connection.User;
@@ -21,6 +22,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.function.Predicate;
 
 import static org.shield.webserver.response.Response.getConnectionErrorResponse;
 import static org.shield.webserver.response.Response.getValidResponse;
@@ -56,6 +61,37 @@ public class CustodianController {
         }
         JsonObject jsonObject = new JsonObject();
         jsonObject.add("custodian", jsonArray);
+        return getValidResponse(jsonObject);
+    }
+    @GetMapping("/settlementBlotter")
+    public ResponseEntity<Response> getSettlementBlotter(@NotNull @RequestBody JsonNode body){
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            User user = objectMapper.readValue(body.get("user").toString(),User.class);
+            generateConnection(user);
+        } catch (IOException e) {
+            return getConnectionErrorResponse(e);
+        }
+
+        JsonArray jsonArray = new JsonArray();
+
+        // for each custodian state, we get the list of trades
+        List<TradeState> tradeStateList = new ArrayList<>();
+        for (StateAndRef<CustodianState> stateAndRef : proxy.vaultQuery(CustodianState.class).getStates()){
+            CustodianState custodianState = stateAndRef.getState().getData();
+            tradeStateList.addAll(custodianState.getTrades());
+        }
+
+        // we generate a new list without duplicates
+        List<TradeState> uniqueTrades = new ArrayList<>(new HashSet<>(tradeStateList));
+        for (TradeState tradeState : uniqueTrades){
+            // we add them to the json result
+            jsonArray.add(tradeState.toJson());
+        }
+
+
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.add("settlementBlotter", jsonArray);
         return getValidResponse(jsonObject);
     }
 }
